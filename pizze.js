17af62c663e1de59ba7810e291b59b1c8009c21a
Marko Šeparović <maseparov@vsite.hr>
2024-05-15 15:52:58 +0200

    dodao pizze.js

---
+++ pizze.js
@@ -0,0 +1,34 @@
+console.log("u pizze.js")
+
+var pizze = {
+    "mijesana": {
+        "naziv": "Miješana",
+        "cijena": 54,
+        "slika": "pizza-mijesana.jpg",
+        "sastojci": ["šunka", "sir", "šampinjoni"]
+    },
+    "margarita": {
+        "naziv": "Margarita",
+        "cijena": 45,
+        "slika": "pizza-margarita.jpg",
+        "sastojci": ["sir"]
+    },
+    "tuna": {
+        "naziv": "Tuna",
+        "cijena": 50,
+        "slika": "pizza-tuna.jpg",
+        "sastojci": ["tuna","luk","maslina","sir"]
+    },
+    "sira4": {
+        "naziv": "4 vrste sira",
+        "cijena": 48,
+        "slika": "pizza-4sira.jpg",
+        "sastojci": ["sir"]
+    },
+    "dubrovnik": {
+        "naziv": "Dubrovnik",
+        "cijena": 53,
+        "slika": "pizza-dubrovnik.jpg",
+        "sastojci": ["pršut","sir","umak od rajčica", "rikula", "mozzarela", "parmezan", "masline"]
+    }
+}
